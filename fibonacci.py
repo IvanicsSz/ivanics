68802c9861bd0b5011bad8bd495a9e445183d9f2
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-05-11 08:53:27 +0200

    input added for sequence

--- fibonacci.py
+++ fibonacci.py
@@ -3,9 +3,11 @@
 import sys
 
 print ("Finonacci first 30 sequence")
+sequence=input("enter the sequence:")
+#fac=factorial(sequence)
 i=1
 f=1
-while (i<31):
+while (i<int(sequence)):
     if (i==1):
         f=f
     else:
