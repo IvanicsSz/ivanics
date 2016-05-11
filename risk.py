899a1a2584bbfbc542d8b67e8e9f139b8f55aa5b
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-05-11 10:08:14 +0200

    risk fully updated

--- risk.py
+++ risk.py
@@ -6,16 +6,43 @@ import functools
 
 def rndNumber():
     return random.randrange(1,7)
+amult=input("attacker dice number:")
+dmult=input("defender dice number")
+#amult=7
+#dmult=7
+attack=[rndNumber()]
+defender=[rndNumber()]
 
-attack=[rndNumber(),rndNumber(),rndNumber()]
-defender=[rndNumber(),rndNumber()]
-#sorted(attack,key=functools.cmp_to_key(reverse_numeric))
-#sorted(defender,key=functools.cmp_to_key(reverse_numeric))
+for i in range(0,int(amult)-1):
+
+    attack.insert(i,rndNumber())
+
+for k in range(0,int(dmult)-1):
+
+    defender.insert(k,rndNumber())
+
+
 attack=sorted(attack,reverse=True)
 defender=sorted(defender,reverse=True)
 
 print (attack,defender)
-if (max(attack)>max(defender)):
-    print ("attacker has win")
+a=0
+d=0
+if (len(attack)>len(defender)):
+    ln=defender
+
 else:
-    print ("defender wins")
+    ln=attack
+
+
+for i in range(len(ln)):
+    if attack[i]>defender[i]:
+        d+=1
+    elif attack[i]<defender[i]:
+        a+=1
+    else:
+        a=a
+        d=d
+
+
+print("attacker lose %i defender lose %i"%(a,d))
