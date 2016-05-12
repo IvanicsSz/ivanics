b03f4c59af6b275611258dcd16f242e7c4221302
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-05-12 08:20:35 +0200

    with dictionary now the best

--- handle.py
+++ handle.py
@@ -3,6 +3,13 @@ import sys
 
 #print ("What is your new idea?")
 idea=input("What is your new idea?")
+#idea="New idea"
 handle=open("handle.txt","wb")
-handle.write(bytes(idea,'UTF-8'))
+
+store={"0.":""}
+store1=store.update({str(len(store))+'. ':idea})
+handle.write(bytes(str(store)+"/n",'UTF-8'))
 handle.close()
+
+for i,k in enumerate(store):
+    print (k,i)
