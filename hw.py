7d7692809e810e1b540cbaa02f07eee13b676701
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-05-12 10:01:03 +0200

    Hello World with multi arg

--- hw.py
+++ hw.py
@@ -7,7 +7,9 @@ def main(argv):
     if (argLen==1):
         print("Hello World")
     elif(argLen>1):
-        print ("Hello "+sys.argv[1]+"!")
-
+        i=0
+        while (i+1<argLen):
+            print ("Hello "+sys.argv[i+1]+"!")
+            i+=1
 if __name__ == "__main__":
    main(sys.argv[1:])
