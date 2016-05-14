084f031297c6bf5829121f1ffc3955994e8e8b7c
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-05-14 09:44:17 +0200

    named tupil example

--- tupil.py
+++ tupil.py
@@ -1,11 +1,14 @@
 import sys,io
-import collections
+import collections as col
 
-a,b = (1,2)
-tup=()
-name="hello world"
-for i in name:
-    name=tuple(name)
-    tup=name.count(i)
-    print (tup)
-print(a,b)
+Sale= col.namedtuple("Sale", "producitid cutomerid date quantity price")
+Sale=(Sale(23,34,"2008-09-19",3,344),Sale(32,11,"2008-09-10",3,200))
+
+sales = list(Sale)
+print (sales)
+#sales.append=(Sale(23,322,"2008-09-19",3,344))
+#sales.append=(Sale(32,112,"2008-09-10",3,200))
+total=0
+for i in sales:
+    total+= i.quantity * i.price
+print(total)
