d8907e24f78f774f25e0e51c0a5971ce2eadea49
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-05-14 10:32:03 +0200

    list extended

--- list.py
+++ list.py
@@ -13,3 +13,12 @@ print(lst)
 for i in range(len(lst)):
     print(i)
     print(lst[i])
+lst.sort()
+print(lst)
+lst.reverse()
+print(lst)
+m=["aa","a","c"]
+lst.extend(m)
+print (lst)
+a=lst.count(1)
+print (a)
